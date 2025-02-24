45c261f10f48ff5e2e9d24cf6d3ed09d886ed6e5
Laiacastle <laia.castillo.7e6@itb.cat>
2025-02-24 12:21:25 +0100

    Created pages. Closes #2

---
+++ EcoEnergySegonaFaseDef/Pages/EnergyIndicators.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EcoEnergySegonaFaseDef.Pages
+{
+    public class EnergyIndicatorsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
