c0572da04bf83d933265c99377f168e5e952ef91
Laiacastle <laia.castillo.7e6@itb.cat>
2025-02-24 13:01:39 +0100

    Created new Pages + starting view simulations

---
+++ EcoEnergySegonaFaseDef/Pages/AddIndicator.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EcoEnergySegonaFaseDef.Pages
+{
+    public class AddIndicatorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ EcoEnergySegonaFaseDef/Pages/AddSimulation.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EcoEnergySegonaFaseDef.Pages
+{
+    public class AddSimulationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
